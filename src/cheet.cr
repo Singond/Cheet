8b9f69c3da5e33856c201ead1b37df2fbc83c2e3
Jan Singon Slany <singond@seznam.cz>
2024-01-11 00:09:53 +0100

    Add main application class

---
+++ src/cheet.cr
@@ -0,0 +1,51 @@
+require "option_parser"
+require "./lib"
+
+include Cheet
+include Cheet::Markdown
+
+module Cheet::Cli
+  extend self
+  Log = ::Log.for "cheet"
+
+  def parse_args(args)
+    area = nil
+    topics = [] of Topic
+    config = Config.new
+    parser = OptionParser.new do |p|
+      p.on "-v", "--verbose", "Increase verbosity" do
+        current_level = Log.level
+        if current_level > ::Log::Severity::Trace
+          Log.level = current_level - 1
+          Log.debug { "Log level set to #{Log.level}" }
+        end
+      end
+      p.unknown_args do |args|
+        if args.size > 1
+          area = parse_area?(args[0])
+        end
+        topics = area ? args[1..] : args
+      end
+    end
+    parser.parse(args)
+    {area, topics, config}
+  end
+
+  private def parse_area?(str : String) : Area?
+    if str.includes?('/')
+      [Path.new(str)]
+    else
+      nil
+    end
+  end
+
+  def main(args = ARGV)
+    area, topics, config_args = parse_args(args)
+    config = Config.layer(config_args, Config.from_env)
+    Cheet.run(area, topics, config)
+  end
+end
+
+Log.define_formatter Fmt, "#{source}: #{message}"
+Log.setup "cheet", :notice, Log::IOBackend.new(STDERR, formatter: Fmt)
+Cheet::Cli.main
