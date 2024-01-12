b156d4e50a6417765ec0c53d0f2c534bc6a27297
Jan Singon Slany <singond@seznam.cz>
2024-01-12 21:42:49 +0100

    Add default argument value

--- src/core.cr
+++ src/core.cr
@@ -62,18 +62,22 @@ module Cheet
       io << @headings
     end
 
-    def next_at_level(start : Int32, level : UInt8) : Heading?
+    def next_at_level(start : Int32, level = level_of(start)) : Heading?
       headings.each
           .skip(start + 1)
           .select { |heading| heading.level == level }
           .first
     end
 
-    def next_above_or_at_level(start : Int32, level : UInt8) : Heading?
+    def next_above_or_at_level(start : Int32, level = level_of(start)) : Heading?
       headings.each
           .skip(start + 1)
           .select { |heading| heading.level <= level }
           .first
+    end
+
+    private def level_of(index)
+      @headings[index]?.try &.level
     end
   end
 
